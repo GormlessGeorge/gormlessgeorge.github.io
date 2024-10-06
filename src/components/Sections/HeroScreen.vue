ca145b7ab0e77640f28f71d94d22a307394951b4
GormlessGeorge <kulaev-george@yandex.ru>
2024-10-06 21:12:37 +0300

    project initialized and set up

---
+++ src/components/Sections/HeroScreen.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>awdwd</h1>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped lang="scss">
+    h1 {
+        color: green;
+        font-weight: 300;
+    }
+</style>
